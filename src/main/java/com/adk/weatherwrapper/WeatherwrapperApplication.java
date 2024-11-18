3f34236fe8d5fe85a8687bc2fe0f9b761a1d0e80
Austin Keiber <adkeiber@gmail.com>
2024-11-18 17:08:35 -0500

    project init

---
+++ src/main/java/com/adk/weatherwrapper/WeatherwrapperApplication.java
@@ -0,0 +1,13 @@
+package com.adk.weatherwrapper;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WeatherwrapperApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WeatherwrapperApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/adk/weatherwrapper/WeatherwrapperApplicationTests.java
@@ -0,0 +1,13 @@
+package com.adk.weatherwrapper;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WeatherwrapperApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
